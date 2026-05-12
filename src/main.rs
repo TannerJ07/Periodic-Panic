93a5d5347e3d165b3da5bcab5d03e63ae189dca0
TannerJ07 <jenkinstj256@gmail.com>
2026-05-12 17:10:20 -0500

    submissions

--- src/main.rs
+++ src/main.rs
@@ -5,12 +5,27 @@ fn main() {
         .insert_resource(ClearColor(Color::srgb(0.4, 0.4, 0.9)))
         .add_plugins(DefaultPlugins)
         .add_systems(Startup, setup)
-        .add_systems(Update, (button_selection, button_system).chain())
+        .add_systems(
+            Update,
+            (button_selection, button_system, run_submission).chain(),
+        )
         .run();
 }
 
 #[derive(Component)]
 struct SelectedOption;
+
+#[derive(Component)]
+struct AnswerButton;
+
+#[derive(Component)]
+struct SubmitButton;
+
+#[derive(Component)]
+struct AnswerIndex(usize);
+
+#[derive(Message)]
+struct RunSubmission;
 
 const BUTTON_COLOR: Color = Color::BLACK;
 const HOVERED_COLOR: Color = Color::srgba(0.7, 0.7, 0.7, 0.7);
@@ -19,7 +34,48 @@ const SELECTED_COLOR: Color = Color::srgba(0.4, 0.9, 0.4, 0.9);
 
 fn setup(mut commands: Commands) {
     commands.spawn(Camera2d);
-    commands.spawn(button());
+    commands.spawn(answer_creation());
+    commands.spawn(submit_button());
+}
+
+fn submit_button() -> impl Bundle {
+    (
+        Node {
+            width: percent(100),
+            height: percent(100),
+            top: percent(40),
+            align_items: AlignItems::Center,
+            justify_content: JustifyContent::Center,
+            ..default()
+        },
+        children![(
+            Button,
+            SubmitButton,
+            Node {
+                align_items: AlignItems::Center,
+                justify_content: JustifyContent::Center,
+                border_radius: BorderRadius::MAX,
+                margin: UiRect::all(px(20)),
+                padding: UiRect {
+                    left: px(15),
+                    right: px(15),
+                    top: px(5),
+                    bottom: px(5),
+                },
+                ..default()
+            },
+            BackgroundColor(BUTTON_COLOR),
+            children![(
+                Text::new("Submit"),
+                TextFont {
+                    font_size: 33.0,
+                    ..default()
+                },
+                TextColor(Color::srgb(0.9, 0.9, 0.9)),
+                TextShadow::default(),
+            ),],
+        )],
+    )
 }
 
 fn button_system(
@@ -37,7 +93,7 @@ fn button_system(
 
 fn button_selection(
     mut commands: Commands,
-    interaction_query: Query<(Entity, &Interaction)>,
+    interaction_query: Query<(Entity, &Interaction), With<AnswerButton>>,
     selected_query: Option<Single<(Entity, &mut BackgroundColor), With<SelectedOption>>>,
 ) {
     for (entity, interaction) in interaction_query {
@@ -52,7 +108,7 @@ fn button_selection(
     }
 }
 
-fn button() -> impl Bundle {
+fn answer_creation() -> impl Bundle {
     (
         Node {
             width: percent(100),
@@ -63,17 +119,19 @@ fn button() -> impl Bundle {
             ..default()
         },
         children![
-            create_button(Text::new("1")),
-            create_button(Text::new("2")),
-            create_button(Text::new("3")),
-            create_button(Text::new("4")),
+            create_button(Text::new("1"), 0),
+            create_button(Text::new("2"), 1),
+            create_button(Text::new("3"), 2),
+            create_button(Text::new("4"), 3),
         ],
     )
 }
 
-fn create_button(text: Text) -> impl Bundle {
+fn create_button(text: Text, index: usize) -> impl Bundle {
     (
         Button,
+        AnswerButton,
+        AnswerIndex(index),
         Node {
             align_items: AlignItems::Center,
             justify_content: JustifyContent::Center,
@@ -98,4 +156,17 @@ fn create_button(text: Text) -> impl Bundle {
             TextShadow::default(),
         ),],
     )
+}
+
+fn run_submission(
+    submit_query: Single<&Interaction, With<SubmitButton>>,
+    selected_query: Option<Single<&AnswerIndex, With<SelectedOption>>>,
+) {
+    if **submit_query == Interaction::Pressed
+        && let Some(selected) = selected_query
+    {
+        if selected.0 == 3 {
+            println!("correct")
+        }
+    }
 }
